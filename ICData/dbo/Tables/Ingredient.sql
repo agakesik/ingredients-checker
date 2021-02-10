4eca615a12a0f01eff83929fa491c5958c832ef8
Aga Kesik <aga.k396@gmail.com>
2021-02-10 15:48:13 +0100

    Create and populate Database Tables: Ingredient and IngredientType

---
+++ ICData/dbo/Tables/Ingredient.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Ingredients]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[name] NVARCHAR(50) NOT NULL, 
+	[details] NCHAR(256) NULL, 
+	[ingredientTypeId] INT NOT NULL, 
+	[isItCG] BIT NOT NULL DEFAULT 0, 
+	CONSTRAINT [FK_Ingredients_ToIngredientType] FOREIGN KEY (ingredientTypeId) REFERENCES IngredientType(Id)
+)
---
+++ ICData/dbo/Tables/IngredientType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[IngredientType]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NCHAR(50) NOT NULL, 
+    [details] NCHAR(256) NULL, 
+    [isItCG] BIT NOT NULL DEFAULT 0, 
+    [color] NCHAR(10) NULL
+)
