8a34d9d18e429170ae00919d7c22645c8777db7c
Aga Kesik <aga.k396@gmail.com>
2021-02-24 13:36:10 +0100

    Move IngredientsCheckerConrtoller into IngredientsController
    Ingredients Checker is doing operations on Ingredients, so it should not be a seperate controller, but a seperate method inside IngredientsController

--- ICDataManager/Controllers/IngredientsController.cs
+++ ICDataManager/Controllers/IngredientsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ICDataManager.Controllers
@@ -17,18 +18,21 @@ namespace ICDataManager.Controllers
         private readonly IIngredientData _ingredientData;
         private readonly IIngredientTypeData _ingredientsTypeData;
         private readonly IDisplayHelper _displayHelper;
+        private readonly IIngredientNameData _ingredientNameData;
 
         public IngredientsController(IIngredientData ingredientData,
                                      IIngredientTypeData ingredientsTypeData,
-                                     IDisplayHelper displayHelper)
+                                     IDisplayHelper displayHelper,
+                                     IIngredientNameData _ingredientNameData)
         {
             _ingredientData = ingredientData;
             _ingredientsTypeData = ingredientsTypeData;
             _displayHelper = displayHelper;
+            this._ingredientNameData = _ingredientNameData;
         }
 
         [HttpGet]
-        public async Task<List<DisplayIngredientModel>> GetAllIngredientsWithTheirTypes()
+        public async Task<List<DisplayIngredientModel>> Get()
         {
 
             var  ingredientsList = await _ingredientData.GetAll();
@@ -39,6 +43,55 @@ namespace ICDataManager.Controllers
 
 
             return detailedIngredientsList;
+        }
+
+        [HttpPost("checker")]
+        public async Task<CheckedIngredientsModel> CheckByNames(IEnumerable<string> names)
+        {
+            if (names == null || !names.Any())
+            {
+                throw new ArgumentNullException("names", "list of names to check is empty");
+            }
+
+            var dbIngredientsList = new List<DBIngredientModel>();
+            var notFoundNames = new List<string>();
+            foreach (var name in names)
+            {
+                var ingredientName = await _ingredientNameData.GetByName(name);
+
+
+                if (ingredientName == null)
+                {
+                    notFoundNames.Add(name);
+                    await _ingredientNameData.SaveName(name);
+                    continue;
+                }
+                else if (ingredientName.IngredientId == 0)
+                {
+                    notFoundNames.Add(name);
+                    continue;
+                }
+
+                var dbIngredient = await _ingredientData.GetById(ingredientName.IngredientId);
+
+                // TODO: there has to be nicer way to get back Ingredients with their searched name, not MainName
+                dbIngredient.MainNameId = ingredientName.Id;
+
+                dbIngredientsList.Add(dbIngredient);
+            }
+
+            var typesList = await _ingredientsTypeData.GetAll();
+
+            List<DisplayIngredientModel> detailedIngredientsList = await _displayHelper.GetIngrediensListForDisplay(dbIngredientsList, typesList);
+
+            var output = new CheckedIngredientsModel
+            {
+                IngredientsList = detailedIngredientsList,
+                NotFoundNames = notFoundNames
+            };
+
+
+            return output;
         }
 
     }
