552c932b06a227294c6918b49f012157ac696da5
Aga Kesik <aga.k396@gmail.com>
2021-03-02 15:01:15 +0100

    Create IngredientsController in MVC that shows list of all ingredients in Index View
    Controller for Admin Panel - creating, updating and deleting Ingredients from database

---
+++ ICDataManager/Controllers/IngredientsController.cs
@@ -0,0 +1,41 @@
+﻿using ICDataManager.Library.Data;
+using ICDataManager.Library.Helpers;
+using ICDataManager.Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICDataManager.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("Admin/[controller]")]
+    public class IngredientsController : Controller
+    {
+        private readonly IIngredientData _ingredientData;
+        private readonly IIngredientTypeData _ingredientsTypeData;
+        private readonly IDisplayHelper _displayHelper;
+
+        public IngredientsController(IIngredientData ingredientData,
+                                     IIngredientTypeData ingredientsTypeData,
+                                     IDisplayHelper displayHelper)
+        {
+            _ingredientData = ingredientData;
+            _ingredientsTypeData = ingredientsTypeData;
+            _displayHelper = displayHelper;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var ingredientsList = await _ingredientData.GetAll();
+            var typesList = await _ingredientsTypeData.GetAll();
+
+            List<DisplayIngredientModel> detailedIngredientsList = await _displayHelper.GetIngrediensListForDisplay(ingredientsList, typesList);
+            return View(ingredientsList);
+        }
+    }
+}
