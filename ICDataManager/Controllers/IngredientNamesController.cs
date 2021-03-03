7125b0e5785a1017ed8cdadd77ed43e166486231
Aga Kesik <aga.k396@gmail.com>
2021-03-03 11:33:51 +0100

    Change Display/Edit Actions in IngredientNames Controller to show names for ingredients instead of their id
    Display now shows the MainName for the Ingredient that the current Name is assigned to. Edit lets user chose from a dropdown list of Ingredients to choose from, instead of typing the Ingredient's Id

--- ICDataManager/Controllers/IngredientNamesController.cs
+++ ICDataManager/Controllers/IngredientNamesController.cs
@@ -1,7 +1,9 @@
 ﻿using ICDataManager.Library.Data;
 using ICDataManager.Library.Models;
+using ICDataManager.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,12 @@ namespace ICDataManager.Controllers
     public class IngredientNamesController : Controller
     {
         private readonly IIngredientNameData _ingredientNameData;
+        private readonly IIngredientData _ingredientData;
 
-        public IngredientNamesController(IIngredientNameData ingredientNameData)
+        public IngredientNamesController(IIngredientNameData ingredientNameData, IIngredientData ingredientData)
         {
             _ingredientNameData = ingredientNameData;
+            _ingredientData = ingredientData;
         }
         
         [HttpGet]
@@ -34,16 +38,34 @@ namespace ICDataManager.Controllers
         [HttpGet("details")]
         public async Task<IActionResult> Details(int id)
         {
-            var name = await _ingredientNameData.GetById(id);
-            return View(name);
+            var ingredients = await _ingredientData.GetAll();
+
+            IngredientNameEditModel model = new IngredientNameEditModel();
+            model.IngredientName = await _ingredientNameData.GetById(id);
+
+
+            // TO DO: there must be cleaner way to do this
+            if (model.IngredientName.IngredientId > 0)
+            {
+                var assignedIngredient = await _ingredientNameData.GetById(ingredients.Where(x => x.Id == model.IngredientName.IngredientId).FirstOrDefault().MainNameId);
+                model.AssignedIngredientMainName = assignedIngredient.Name;
+            }
+
+            foreach (var ingredient in ingredients)
+            {
+                var ingredientMainName = await _ingredientNameData.GetById(ingredient.MainNameId);
+                model.Ingredients.Add(new SelectListItem { Value = ingredient.Id.ToString(), Text = ingredientMainName.Name });
+            }
+
+            return View(model);
         }
 
         [HttpPost("update")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Update(DBIngredientNameModel editedInformation)
+        public async Task<IActionResult> Update(IngredientNameEditModel editModel)
         {
-            await _ingredientNameData.Update(editedInformation);
-            return RedirectToAction("Details", new { editedInformation.Id });
+            await _ingredientNameData.Update(editModel.IngredientName);
+            return RedirectToAction("Details", new { editModel.IngredientName.Id });
         }
 
         [HttpPost]
---
+++ ICDataManager/Models/IngredientNameEditModel.cs
@@ -0,0 +1,17 @@
+﻿using ICDataManager.Library.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICDataManager.Models
+{
+    public class IngredientNameEditModel
+    {
+        public DBIngredientNameModel IngredientName { get; set; }
+
+        public string AssignedIngredientMainName { get; set; } = "";
+        public List<SelectListItem> Ingredients { get; set; } = new List<SelectListItem>();
+    }
+}
