873341c48bcd55efe26a7354365a66603a8f820d
Aga Kesik <aga.k396@gmail.com>
2021-03-04 13:08:02 +0100

    Create IngredientType Controller for Admin Panel

---
+++ ICDataManager/Controllers/IngredientTypesController.cs
@@ -0,0 +1,29 @@
+﻿using ICDataManager.Library.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICDataManager.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("Admin/[controller]")]
+    public class IngredientTypesController : Controller
+    {
+        private readonly IIngredientTypeData _ingredientsTypeData;
+
+        public IngredientTypesController(IIngredientTypeData ingredientsTypeData)
+        {
+            _ingredientsTypeData = ingredientsTypeData;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var typesList = await _ingredientsTypeData.GetAll();
+
+            return View(typesList);
+        }
+    }
+}
